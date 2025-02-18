6a4931286867cc87bb968d4796704646c3640a0b
Shusuke Nakamura <shusuke.pana@gmail.com>
2025-02-18 19:54:38 +0900

    chap02の学習完了

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -3,13 +3,128 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AppClass from './AppClass';
+import './chap02/class.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// リスト2-2-4
+// setInterval(() => {
+//   root.render(
+//     <p>現在時刻：{ (new Date()).toLocaleString() }</p>
+//   );
+// }, 1000);
+
+// root.render(
+//   <React.StrictMode>
+//     <AppClass />
+//   </React.StrictMode>
+// );
+
+// root.render(
+//   <>
+//     <p>こんにちは、世界！</p>
+//     <p>はじめまして、React!!</p>
+//   </>
+// );
+
+// const name = '鈴木';
+// root.render(
+//   <p>こんにちは、{name}です！</p>
+// );
+
+// const content = `<h3>WINGSプロジェクト</h3>
+//   <img src="https://wings.msn.to/image/wings.jpg" />`;
+// root.render(
+//   <p>{content}</p>
+//   // <p dangerouslySetInnerHTML={{__html: content}}></p>
+// );
+
+// root.render(
+//   <div>{'Tom &amp; Jerry'}</div>
+//   // <div>Tom &amp; Jerry</div>
+//   // <div>{'Tom \u0026 Jerry'}</div>
+//   // <div>{`Tom ${String.fromCodePoint(38)} Jerry`}</div>
+//   // <div dangerouslySetInnerHTML={{__html: 'Tom &amp; Jerry'}} />
+// );
+
+// root.render(
+//   <ul>
+//     <li>{true}</li>
+//     <li>{false}</li>
+//     <li>{undefined}</li>
+//     <li>{null}</li>
+//     <li>{0}</li>
+//   </ul>
+// );
+
+// const dest = 'https://ja.react.dev';
+// root.render(
+//   <a href={dest}>React本家サイト</a>
+// );
+
+// const attrs = {
+//   href: 'https://wings.msn.to/',
+//   download: false,
+//   target: '_blank',
+//   rel: 'help'
+// }
+
+// root.render(
+//   // <a href={attrs.href} download={attrs.download} target={attrs.target} rel={attrs.rel}>サポートサイトへ</a>
+//   <a {...attrs}>サポートサイトへ</a>
+// );
+
+// const props = 'color: White; background: Blue; padding: 3px';
+// const props = {
+//   color: 'White',
+//   background: 'Blue',
+//   padding: '3px'
+// };
+
+// root.render(
+//   <p style={props}>WINGSプロジェクト</p>
+// );
+
+// root.render(
+//   <p className='hoge'>WINGSプロジェクト</p>
+// );
+
+// const title = 'これからはじめるVue.js 3実践入門';
+
+// root.render(
+//   <div className='main'>
+//     <p>「{title}」（SBクリエイティブ刊）</p>
+//     <img src="https://wings.msn.to/books/978-4-8156-1336-5/978-4-8156-1336-5.jpg" alt={title} />
+//     絶賛発売中！
+//   </div>
+// );
+
+// root.render(
+//   React.createElement(
+//     'div',
+//     { className: 'main' },
+//     React.createElement(
+//       'p',
+//       null,
+//       `「${title}」（SBクリエイティブ刊）`
+//     ),
+//     React.createElement(
+//       'img',
+//       {
+//         src: 'https://wings.msn.to/books/978-4-8156-1336-5/978-4-8156-1336-5.jpg' ,
+//         alt: title
+//       }
+//     ),
+//     '絶賛発売中！'
+//   )
+// );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
