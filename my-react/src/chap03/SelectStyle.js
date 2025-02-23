59021607301234d1207d203140ef11d07b0ce21a
Shusuke Nakamura <shusuke.pana@gmail.com>
2025-02-24 08:25:35 +0900

    classnamesライブラリを使用に変更

--- my-react/src/chap03/SelectStyle.js
+++ my-react/src/chap03/SelectStyle.js
@@ -1,8 +1,12 @@
 import './SelectStyle.css';
+import cn from 'classnames';
 
 export default function SelectStyle({mode}) {
     return (
-        <div className={`box ${mode === 'light' ? 'light' : 'dark'}`}>
+        // <div className={`box ${mode === 'light' ? 'light' : 'dark'}`}>
+        //     こんにちは、世界！
+        // </div>
+        <div className={cn('box', mode === 'light' ? 'light' : 'dark')}>
             こんにちは、世界！
         </div>
     );
