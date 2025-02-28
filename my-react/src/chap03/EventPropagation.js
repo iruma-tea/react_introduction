69ba507724c6152c0a16dced0e6098a9cf3bbe49
Shusuke Nakamura <shusuke.pana@gmail.com>
2025-02-28 20:18:09 +0900

    イベントの規定の動作をキャンセルするまで学習完了

--- my-react/src/chap03/EventPropagation.js
+++ my-react/src/chap03/EventPropagation.js
@@ -4,6 +4,14 @@ export default function EventPropagation() {
     const handleParent = () => alert('#parent run...');
     const handleMy = () => alert('#my run...');
     const handleChild = () => alert('#child run...');
+    // const handleChild = e => {
+    //     e.stopPropagation();
+    //     alert('#child run...')
+    // };
+    // const handleChild = e => {
+    //     e.preventDefault();
+    //     alert('#child run...')
+    // };
 
     return (
         <div id='parent' onClick={handleParent}>
@@ -16,4 +24,15 @@ export default function EventPropagation() {
             </div>
         </div>
     );
+    // return (
+    //     <div id='parent' onClickCapture={handleParent}>
+    //         親要素
+    //         <div id='my' onClickCapture={handleMy}>
+    //             現在要素
+    //             <a id='child' href='https://wings.msn.to' onClickCapture={handleChild}>
+    //                 子要素
+    //             </a>
+    //         </div>
+    //     </div>
+    // );
 }
